ce950d584296d5bd26d55d72a441e8c834561ee0
Steven Tardiff <tardiff.steven@gmail.com>
2020-10-26 11:26:53 -0400

    admin product lsit screen/ delete product

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -5,6 +5,9 @@ import {
   PRODUCT_DETAILS_FAIL,
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DELETE_REQUEST,
+  PRODUCT_DELETE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
 } from '../constants/productConstants';
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -47,6 +50,30 @@ export const productDetailsReducer = (state = { product: {} }, action) => {
       };
 
     case PRODUCT_DETAILS_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export const productDeleteReducer = (state = { product: {} }, action) => {
+  switch (action.type) {
+    case PRODUCT_DELETE_REQUEST:
+      return {
+        loading: true,
+      };
+
+    case PRODUCT_DELETE_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+      };
+
+    case PRODUCT_DELETE_FAIL:
       return {
         loading: false,
         error: action.payload,
